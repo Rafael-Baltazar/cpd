8a056670c5e999393e6cd2d30c8b3e5270a9c67e
ASourLemon <nelsonflsilva@gmail.com>
2013-10-17 12:27:44 +0100

    Added dynamic world criation

--- wolves-squirrels-serial.c
+++ wolves-squirrels-serial.c
@@ -5,8 +5,7 @@
  */
 
 #include <stdio.h>
-
-#define MAX 20	
+#include <stdlib.h>	
 
 //Types (Defined as binnary masks to make the comparations easier to handle)
 // Empty is 0
@@ -18,16 +17,20 @@
 
 #define RED 0
 #define BLACK 1
+/*
+ * VERY DIRTY HACK, GLOBAL VARIABLES
+ */
+ //size of the world
+ int max_size;
 
 /*
  * World structure
  */
-
 struct world {
 	int type; /* Wolf, Squirrel, Ice, Tree (Empty is 0) */
  	int breeding_period;
  	int starvation_period;
- } world[MAX][MAX];
+ } **world;
  
  
 //functions for animal behavior
@@ -47,9 +50,8 @@ void compute_squirrel(int row, int col){
  */
 void iterate_subgeneration(int color) {
 	int i, j;
-	
-	for(i = 0; i < MAX; i++) {
-		for(j = color; j < MAX; j += 2) {
+	for(i = 0; i < max_size; i++) {
+		for(j = color; j < max_size; j += 2) {
 			if(world[i][j].type & WOLF)
 				;//update_wolf(i,j);
 			else if(world[i][j].type & SQUIRREL)
@@ -67,8 +69,8 @@ void print_all_cells(){
 	char type;
 	struct world cell;
 
-	for(i = 0; i < MAX; i++) {
-		for(j = 0; j < MAX; j++) {
+	for(i = 0; i < max_size; i++) {
+		for(j = 0; j < max_size; j++) {
 			cell = world[i][j];
 
 			if(cell.type) {
@@ -106,18 +108,33 @@ void populate_world_from_file(char file_name[]){
 	if(fp == NULL) {
 		printf("Error while opening the file.\n");
 	} else {
-		printf("File found. Now polulating...\n");
-		int i, j, max_size;
+		printf("File found. Now populating...\n");
+		int i, j, k;
 		char a;
+		fscanf(fp, "%d", &max_size);
+		world = (struct world**) malloc(max_size*sizeof(*world));
+		for(k=0;k<max_size;++k) {
+			world[k] = (struct world*) malloc(max_size*sizeof(struct world));
+		}
+		for(i=0;i<max_size;++i) {
+			for(j=0;j<max_size;++j){
+				world[i][j].type = 0;
+			}
+		}
+		
 		while(fscanf(fp, "%d %d %c", &i, &j, &a) != EOF) {
 			if(a=='w')
-				world[j][i].type = WOLF;
+				world[i][j].type = WOLF;
 			else if(a=='s')
-				world[j][i].type = SQUIRREL;
+				world[i][j].type = SQUIRREL;
 			else if(a=='i')
-				world[j][i].type = ICE;
+				world[i][j].type = ICE;
 			else if(a=='t')
-				world[j][i].type = TREE;
+				world[i][j].type = TREE;
+			else if(a=='$')
+				world[i][j].type = SQUIRREL | TREE;
+			else 
+				world[i][j].type = 0;	
 		}
 	}
 }
@@ -129,12 +146,6 @@ void populate_world_from_file(char file_name[]){
 int main(int argc, char **argv) {
 	//Test world print
 
-	world[0][0].type = WOLF;
-	world[1][2].type = SQUIRREL | TREE;
-	world[2][2].type = ICE;
-	world[3][1].type = TREE;
-	world[1][0].type = WOLF | SQUIRREL;	
-	print_all_cells(world);
 
 	int w_breeding_p, s_breeding_p, w_starvation_p, num_gen;
 	if(argc>1) {
@@ -144,7 +155,6 @@ int main(int argc, char **argv) {
 		w_starvation_p = atoi(argv[4]);
 		num_gen = atoi(argv[5]);	
 	}
-	print_all_cells();
-
+	print_all_cells(world);
 	return 0;	
 }
